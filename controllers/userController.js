d9eba827d2324daa4b2ff984767722bf99579ebd
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-03-17 16:38:59 +0530

    user profile api is connected

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-
-const bcrypt= require("bcrypt")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const mysql = require("mysql");
-const catchAsyncErros = require("../middleware/catchAsyncErrors");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const salt = 10;
 
 const db = mysql.createConnection({
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
 
 //get all user info
 
-exports.getUsers = catchAsyncErros(async (req, res, next) => {
+exports.getUsers = catchAsyncErrors(async (req, res, next) => {
   const sql = "SELECT * FROM users";
   db.query(sql, (err, data) => {
     if (err) {
@@ -25,10 +25,11 @@ exports.getUsers = catchAsyncErros(async (req, res, next) => {
 
 //signup/ register
 
-exports.signUp = catchAsyncErros(async (req, res, next) => {
+exports.signUp = catchAsyncErrors(async (req, res, next) => {
+  const saltRounds = 10;
   const sql =
     "INSERT INTO users(`name`, `email`, `phone`, `password`) VALUES (?, ?, ?, ?)";
-  bcrypt.hash(req.body.password, salt, (err, hash) => {
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
     if (err) {
       console.log(err);
     }
@@ -46,17 +47,67 @@ exports.signUp = catchAsyncErros(async (req, res, next) => {
 
 //login
 
-exports.login = catchAsyncErros(async (req, res, next) => {
-  const sql = "SELECT * FROM users WHERE `email` =? AND `password`=?";
-  db.query(sql, [req.body.email, req.body.password], (err, data) => {
-    console.log(err);
+exports.login = catchAsyncErrors(async (req, res, next) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  if (email && password) {
+    const sql = "SELECT * FROM users WHERE `email` =?";
+    db.query(sql, [email], async (err, data) => {
+      if (err) {
+        return res.json("login custom Error by me");
+      }
+
+      if (data.length > 0) {
+        const user = data[0];
+
+        // Compare hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+          // Passwords match, create token
+          const token = jwt.sign({ userId: user.id }, "sdsdsdsdsdsdsdsd", {
+            expiresIn: "1h",
+          });
+
+          // Send the token in response
+          return res.json({ success: true, token: token });
+        } else {
+          return res.json({ success: false, message: "Incorrect password" });
+        }
+      } else {
+        return res.json({ success: false, message: "User not found" });
+      }
+    });
+  } else {
+    res.json({ success: false, message: "Email and password are required" });
+  }
+});
+
+//get user profile
+
+exports.getProfile = catchAsyncErrors(async (req, res, next) => {
+  console.log(req.user)
+  const userId = req.user.userId
+  const sql = "SELECT * FROM users WHERE `id`=? ";
+  db.query(sql, [userId],(err, [data]) => {
     if (err) {
-      return res.json("login custom Error by me");
+      return res.json("Error");
     }
-    if (data.length > 0) {
-      return res.json("success");
-    } else {
-      return res.json("fail");
-    }
+    return res.json(data);
   });
 });
+
+exports.authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"]; //
+  const token = authHeader && authHeader.split(" ")[1]; //Bearer token,
+  if (!token) {
+    return res.status(401).json("Un-authorized");
+  }
+  jwt.verify(token, "sdsdsdsdsdsdsdsd", (error, user) => {
+    if (error) {
+      return res.status(403).json("forbidden");
+    }
+    req.user = user;
+    next();
+  });
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { getUsers, signUp, login } = require("../controllers/userController");
+const {
+  getUsers,
+  signUp,
+  login,
+  getProfile,
+  authenticateToken,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.route("/users").get(getUsers);
 router.route("/signup").post(signUp);
-router.route("/login").post(login)
-
+router.route("/login").post(login);
+router.route("/profile").get(authenticateToken, getProfile);  //if authentication is succeed and user is found then getProfile will execute
 
 module.exports = router;
