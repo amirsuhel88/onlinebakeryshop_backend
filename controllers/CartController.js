a8a5471a8a1ffa0c599b11b1ae3f7b4507971fa7
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-04-25 16:50:24 +0530

    cart is getting all the info from all the tables

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,3 +1,5 @@
+const express = require("express")
+//const rotuer = express.Router();
 const mysql = require("mysql");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const jwt = require("jsonwebtoken");
@@ -11,24 +13,12 @@ const db = mysql.createConnection({
   database: "onlinebakeryshop",
 });
 
-//get all cart items
-
-exports.getCartItems = catchAsyncErrors(async (req, res, next) => {
-  const sql = "SELECT * FROM cart";
-  db.query(sql, (err, data) => {
-    if (err) {
-      return res.json("Error");
-    }
-    return res.json(data);
-  });
-});
-
 //add to cart
 exports.addToCart = catchAsyncErrors(async (req, res, next) => {
   console.log(req.user);
   const userId = req.user.userId;
   const productId = req.params.productId;
-  console.log(productId)
+  console.log(productId);
   // Check if the product with given productId exists
   const productExistsQuery = "SELECT * FROM products WHERE ProductId = ?";
   db.query(productExistsQuery, [productId], async (err, results) => {
@@ -56,5 +46,46 @@ exports.addToCart = catchAsyncErrors(async (req, res, next) => {
         .status(500)
         .json({ success: false, error: "Failed to add item to cart" });
     }
+  });
+});
+
+//get all cart items(show cart items)
+
+/*
+exports.getCartItems = catchAsyncErrors(async (req, res, next) => {
+  const sql = "SELECT * FROM cart";
+  db.query(sql, (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    return res.json(data);
+  });
+});
+ */
+
+exports.getCartItems = catchAsyncErrors(async (req, res, next) => {
+  //retrieve user ID from req.user(which contains user)
+  const userId = req.user.userId;
+
+  //sql query to fetch cart items and associated product details for the logged-in user
+  const sql = `
+  SELECT 
+    p.ProductId,
+    p.ProductName,
+    p.description,
+    p.Price,
+    p.CategoryName,
+    c.quantity
+    from cart c
+    JOIN products p ON c.ProductID = p.ProductId
+    WHERE
+      c.UserId = ?
+ `;
+  db.query(sql, [userId], (err, data) => {
+    if (err) {
+      return res.status(500).json("internal Server Error");
+    }
+    //respond with the fetched cart items and product details
+    res.json(data);
   });
 });
--- routes/CartRoute.js
+++ routes/CartRoute.js
@@ -3,6 +3,6 @@ const {addToCart, getCartItems} = require("../controllers/CartController");
 const {authenticateToken} = require("../controllers/userController");
 const router = express.Router();
 
-router.route("/cartItems").get(getCartItems);
+router.route("/cartItems").get(authenticateToken,getCartItems);
 router.route("/addToCart/:productId").post(authenticateToken, addToCart);
 module.exports = router;
